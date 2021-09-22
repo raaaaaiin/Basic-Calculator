809c0e92f64b7471d41f02889f079fe0680df042
raaaaaiin <marcraineer0089@gmail.com>
2021-09-23 04:18:56 +0800

    Initial Commit

---
+++ Indux.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Putanidamo</title>
+		<link rel="stylesheet" href="Script/bootstrap.min.css">
+		<link rel="stylesheet" href="Script/Style.css">
+		<script src="Script/jquery.min.js"></script>
+		<script src="Script/bootstrap.js"></script>
+		<script src="Script/index-functions.js"></script>
+	</head>
+	<body>
+
+
+
+	
+	</body>
+	<script>
+	</script>
+</html>
